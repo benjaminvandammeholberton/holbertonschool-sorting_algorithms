c56b329015a367019f3abb36d2721b66dd10e366
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-05-23 14:45:07 +0200

    Add: two firsts tasks of the project with their Big O notations files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - This function sorts an array of integers in ascending
+ *               order using the Bubble sort algorithm
+ * @array: the pointer to the array
+ * @size: the size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j = size - 1;
+	int temp = 0;
+
+	while (j >= 1)
+	{
+		i = 0;
+		while (i < j)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = temp;
+				print_array(array, size);
+			}
+			i++;
+		}
+		j--;
+	}
+}
